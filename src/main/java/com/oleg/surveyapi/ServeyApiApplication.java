ed302b241ffb2cc66748414a3f45a4bf4521d04c
Олег Романов <romanovolegd@gmail.com>
2021-09-15 20:09:33 +0300

    add security

--- src/main/java/com/oleg/surveyapi/ServeyApiApplication.java
+++ src/main/java/com/oleg/surveyapi/ServeyApiApplication.java
@@ -1,9 +1,12 @@
 package com.oleg.surveyapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Surveys API", description = "API for making surveys"))
 public class ServeyApiApplication {
 
 	public static void main(String[] args) {
